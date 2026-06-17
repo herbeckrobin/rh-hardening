0cb05cc69a24c27545719722a824207b8e52006b
Robin Herbeck <info@robinherbeck.com>
2026-06-17 09:57:17 +0200

    Core 2.3.0 (Suite-Erweiterung), Version 0.1.1

--- rh-hardening.php
+++ rh-hardening.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/herbeckrobin/rh-hardening
  * Update URI:        https://github.com/herbeckrobin/rh-hardening
  * Description:       Security-Hardening für WordPress: User-Enumeration blocken, Feeds aus, Security-Header, WP-Schmodder und XML-RPC raus. Teil der rh-blueprint Kollektion.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Robin Herbeck
@@ -21,7 +21,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('RHHARD_VERSION', '0.1.0');
+define('RHHARD_VERSION', '0.1.1');
 define('RHHARD_PLUGIN_FILE', __FILE__);
 define('RHHARD_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
